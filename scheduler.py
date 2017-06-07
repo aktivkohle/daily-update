5772322446039db81c109dd905d18b637df79efb
Felix Snow <felix.s@posteo.de>
2017-06-07 20:16:12 +0200

    cleaned up scheduler.py

--- scheduler.py
+++ scheduler.py
@@ -4,17 +4,19 @@ from time import sleep
 from scrape_and_send import scrape_send
 
 
+# delay the start a random time after the Scheduler to make scraping less obvious
 def randStart():
-	# for testing use this line:
-    randsecs = randrange(1000)   # random in 1000 seconds	
-	# for production use this line:	
-    # randsecs = randrange(3600)
+    randsecs = randrange(1000)   # random number of seconds delay between 1 and 1000
     print(randsecs)
     sleep(randsecs)
     # print('running.')
     scrape_send()
     
-# http://apscheduler.readthedocs.io/en/latest/modules/triggers/cron.html#module-apscheduler.triggers.cron
 sched = BlockingScheduler()
-sched.add_job(randStart, trigger='cron', hour='17', minute='45') # platform uses UTC time (2 hours behind)
+sched.add_job(randStart, trigger='cron', hour='17', minute='45') # host platform uses UTC time (2 hours behind)
+# for adjusting the cron job see this link:
+# http://apscheduler.readthedocs.io/en/latest/modules/triggers/cron.html#module-apscheduler.triggers.cron
 sched.start()
+
+
+
