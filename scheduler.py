b409a072f1898b375906446ec137b0d04056cd30
felix <felix.s@posteo.de>
2017-06-12 11:47:37 +0200

    switch to backgroundscheduler

--- scheduler.py
+++ scheduler.py
@@ -1,4 +1,4 @@
-from apscheduler.schedulers.background import BlockingScheduler
+from apscheduler.schedulers.background import BackgroundScheduler
 from random import randrange
 from time import sleep
 from scrape_and_send import scrape_send
@@ -6,15 +6,15 @@ from scrape_and_send import scrape_send
 
 # delay the start a random time after the Scheduler to make scraping less obvious
 def randStart():
-    randsecs = randrange(1500)   # random number of seconds delay between 1 and 1500
+    randsecs = randrange(3600)   # random number of seconds delay between 1 and 3600
     print(randsecs)
     sleep(randsecs)
     # print('running.')
     scrape_send()
     
-sched = BlockingScheduler()
-sched.add_job(randStart, trigger='cron', hour='18', minute='1') # host platform uses UTC time (2 hours behind)
-sched.add_job(randStart, trigger='cron', hour='10', minute='1') # host platform uses UTC time (2 hours behind)
+sched = BackgroundScheduler()
+sched.add_job(randStart, trigger='cron', hour='18', minute='10') # host platform uses UTC time (2 hours behind)
+sched.add_job(randStart, trigger='cron', hour='10', minute='10') # host platform uses UTC time (2 hours behind)
 # for adjusting the cron job see this link:
 # http://apscheduler.readthedocs.io/en/latest/modules/triggers/cron.html#module-apscheduler.triggers.cron
 sched.start()
